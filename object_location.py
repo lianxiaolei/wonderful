04d3f7b956f6c451157c20888718510097e5efd8
连晓磊 <lianxiaolei@hqyxjy.com>
2018-03-19 23:12:52 +0800

    一直出JB些扯蛋的东西

--- object_location.py
+++ object_location.py
@@ -10,26 +10,20 @@ import os
 import sys
 
 
-def get_common_square(r0, r1):
-    minx = max(r0[0], r1[0])
-    miny = max(r0[1], r1[1])
-    maxx = min(r0[0] + r0[2], r1[0] + r1[2])
-    maxy = min(r0[1] + r0[3], r1[1] + r1[3])
-    return (maxy - miny) * (maxx - minx)
-
-
-def nms(r):
-    for i in xrange(len(r)):
-        if r[i][2] - r[i][0] > 200 or r[i][3] - r[i][1] > 100:
-            continue
-        for j in xrange(len(r)):
-            if r[j][0]:
-                pass
-            np.nonzero()
-
-
 # IOU Part 1
 def if_intersection(xmin_a, xmax_a, ymin_a, ymax_a, xmin_b, xmax_b, ymin_b, ymax_b):
+    """
+    Determine whether the two matrices cross
+    :param xmin_a:
+    :param xmax_a:
+    :param ymin_a:
+    :param ymax_a:
+    :param xmin_b:
+    :param xmax_b:
+    :param ymin_b:
+    :param ymax_b:
+    :return:
+    """
     if_intersect = False
     # 通过四条if来查看两个方框是否有交集。如果四种状况都不存在，我们视为无交集
     if xmin_a < xmax_b <= xmax_a and (ymin_a < ymax_b <= ymax_a or ymin_a <= ymin_b < ymax_a):
@@ -53,6 +47,12 @@ def if_intersection(xmin_a, xmax_a, ymin_a, ymax_a, xmin_b, xmax_b, ymin_b, ymax
 
 
 def get_IOU(r0, r1):
+    """
+    Get the IOU of the two crossed matrixs
+    :param r0:
+    :param r1:
+    :return:
+    """
     ver0 = [r0[0], r0[0] + r0[2], r0[1], r0[1] + r0[3]]
     ver1 = [r1[0], r1[0] + r1[2], r1[1], r1[1] + r1[3]]
     ver_long = ver0 + ver1
@@ -65,23 +65,20 @@ def get_IOU(r0, r1):
     return 0, 0, 0
 
 
-def selective_search(file_name):
-    img = cv2.imread(file_name)
-    h = img.shape[0]
-    w = img.shape[1]
+def selective_search(img1, min_size=100):
+    """
 
-    if h > 1000 and w > 1000:
-        w = w / 10
-        h = h / 10
-        img = cv2.resize(img, (w, h))
-    ret, img1 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
-
+    :param img1:
+    :param min_size:
+    :return:
+    """
     # 去除大片的空白区域
     # img1 = img1[np.sum(np.sum(img1, axis=1), axis=1) != 0,:,:]
 
+    w, h, _ = img1.shape
     img_lbl, regions = selectivesearch.selective_search(
-        img1, scale=400, sigma=1.8, min_size=100)
-    print 'region number', len(regions)
+        img1, scale=400, sigma=1.8, min_size=min_size)
+    # print 'region number', len(regions)
     print regions
 
     sort_key = lambda item: -item['rect'][2] * item['rect'][3]
@@ -89,16 +86,26 @@ def selective_search(file_name):
     regions = sorted(regions, key=sort_key)
     candidates = []
     for i in xrange(len(regions) - 1):
+        print '+' * 80
+        print regions[i]['rect'][2] * regions[i]['rect'][3], regions[i]['rect'][2]
         correct = regions[i]['rect']
         if regions[i]['rect'] in candidates:
+            print candidates
             continue
         if abs(regions[i]['rect'][2] - w) < 100:
+            print '大', regions[i]['rect'][2] * regions[i]['rect'][3], min_size
             continue
+        if regions[i]['rect'][2] * regions[i]['rect'][3] < min_size:
+            print '小', regions[i]['rect'][2] * regions[i]['rect'][3], min_size
+            continue
+        print regions[i]['rect'][2] * regions[i]['rect'][3], regions[i]['rect'][2]
+        print '-' * 80
         for j in xrange(i + 1, len(regions)):
             if regions[j]['rect'] in candidates:
-                # print 'there is a repeat region'
                 continue
             if abs(regions[j]['rect'][2] - w) < 100:
+                continue
+            if regions[j]['rect'][2] * regions[i]['rect'][3] < min_size:
                 continue
             IOU, square0, square1 = get_IOU(regions[i]['rect'], regions[j]['rect'])
             # print regions[i]['rect'], regions[j]['rect'], square0, square1, IOU
@@ -123,13 +130,35 @@ def selective_search(file_name):
             patches.Rectangle(
                 (candidates[i][0], candidates[i][1]),
                 candidates[i][2], candidates[i][3],
-                linewidth=1, edgecolor='r', facecolor='none'))
+                linewidth=1, edgecolor='g', facecolor='none'))
     plt.show()
 
 
+def read_img(file_name):
+    """
+    read a image from local file system
+    :param file_name:
+    :return:
+    """
+    img = cv2.imread(file_name)
+    h = img.shape[0]
+    w = img.shape[1]
+    if h > 1000 and w > 1000:
+        w = w / 10
+        h = h / 10
+        img = cv2.resize(img, (w, h))
+    ret, img1 = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
+    return img1
+
+
+def run(file_name):
+    img = read_img(file_name)
+    selective_search(img, 300)
+
+
 if __name__ == '__main__':
-    # selective_search('images/11.png')
-    selective_search('images/12.jpg')
-    selective_search('images/24.jpg')
-    selective_search('images/13.png')
-    # selective_search('images/14.jpg')
+    # run('images/11.png')
+    # run('images/12.png')
+    # run('images/24.jpg')
+    # run('images/13.png')
+    run('images/14.jpg')
